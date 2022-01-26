5da3b40818e881ffaf1f82daedec47dcd9cff686
michalsuder <m.suder2@gmail.com>
2022-01-26 14:31:03 +0100

    Fix formatting

--- BlazorFileParser/BlazorFileParser/Data/EventRecord.cs
+++ BlazorFileParser/BlazorFileParser/Data/EventRecord.cs
@@ -19,19 +19,19 @@ namespace BlazorFileParser.Data
         {
             if (string.IsNullOrWhiteSpace(Name))
                 throw new Exception($"Error at line {e.LineNumber}. Event name cannot be empty");
-            
+
             if (Name.Length > maxNameLength)
                 throw new Exception($"Error at line {e.LineNumber}. Event name cannot be longer than {maxNameLength} characters");
-            
+
             if (string.IsNullOrWhiteSpace(Description))
                 throw new Exception($"Error at line {e.LineNumber}. Event description cannot be empty");
-            
+
             if (Description.Length > maxDescriptionLength)
                 throw new Exception($"Error at line {e.LineNumber}. Event description cannot be longer than {maxDescriptionLength} characters");
         }
 
         public void BeforeRead(BeforeReadEventArgs e)
-        { 
+        {
             if (e.RecordLine.EndsWith(';'))
             {
                 e.RecordLine = e.RecordLine.TrimEnd(';');
